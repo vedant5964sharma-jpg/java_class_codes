8ea869fd649f64464e064dfa8b03d8bc04cada7e
vedant sharma <vedantsharma@vedants-MacBook-Air-4.local>
2026-02-23 10:58:17 +0530

    Initial commit - only java source files

---
+++ ThrowsExample.java
@@ -0,0 +1,10 @@
+public class ThrowsExample {
+    public static void main(String[] args) {
+        
+        int n1=10;
+        int n2=0;
+        int n3=n1/n2;
+        System.out.println(n3);
+    }
+    
+}
